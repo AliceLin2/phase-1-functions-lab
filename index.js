ce2e361bdb1583de612b0a89ed5debdd2b4bb52d
shu Lin <bbqlinshu@gmail.com>
2021-11-23 23:02:56 -0500

    commit tree c0be5db578e69aa141915c5b24e8f57641fcfb12

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNum){
+    return Math.abs(streetNum - 42);
+}
+
+function distanceFromHqInFeet(streetNum){
+    return Math.abs(streetNum - 42)*264;
+}
+
+function distanceTravelledInFeet(street1, street2){
+    return Math.abs(street1 - street2)*264;
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = Math.abs(start - destination)*264;
+    let fare;
+    if (distance <= 400){
+        return fare = 0;
+    } else if ( distance > 400 && distance <= 2000){
+        return fare =0.02*(distance-400);
+    } else if ( distance > 2000 && distance <= 2500) {
+        return fare =25;
+    } else {
+        return 'cannot travel that far';
+    }
+
+}
